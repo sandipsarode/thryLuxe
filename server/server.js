aba7d57e4d767c3286ebc651d9cae13da294dc70
sandipsarode <sandiipssarode@gmail.com>
2025-05-09 17:01:25 +0530

    Added Allowed Origin

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 4000;
 await connectDB();
 await connectCloudinary();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", "https://thryluxe.vercel.app"];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
