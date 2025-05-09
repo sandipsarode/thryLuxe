8cf12596c76e2c9373f1ff9ea0caa9cd7f69392c
sandipsarode <sandiipssarode@gmail.com>
2025-05-09 12:33:22 +0530

    npm run build

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useAppContext } from "../../context/AppContext";
-import axios from "axios";
+
 import toast from "react-hot-toast";
 
 const SellerLogin = () => {
