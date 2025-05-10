f589ee11e02a8f61aca9eef9723282f7e889982f
sandipsarode <sandiipssarode@gmail.com>
2025-05-10 18:10:58 +0530

    Working on Contact Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,7 @@ import AddProduct from "./pages/seller/AddProduct";
 import ProductList from "./pages/seller/ProductList";
 import Orders from "./pages/seller/Orders";
 import Loading from "./components/Loading";
+import Contact from "./pages/Contact";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -35,6 +36,7 @@ const App = () => {
           <Route path="/products" element={<AllProducts />} />
           <Route path="/products/:category" element={<ProductCategory />} />
           <Route path="/products/:category/:id" element={<ProductDetails />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/addaddress" element={<AddAddress />} />
           <Route path="/myorders" element={<MyOrders />} />
